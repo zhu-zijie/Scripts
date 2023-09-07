92d69954ba804916f493bdff951fb770e068f54a
不相交直线 <68392315+zhu-zijie@users.noreply.github.com>
2023-09-07 20:41:05 +0800

    Add files via upload
    Java的第一行代码!

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        //System.out.println("HelleWorld!");
+        int i = 128;
+        byte j = (byte)i;
+        System.out.println(j);
+    }
+}
