dd926eb2d69106ec8db2ffce65876da699e3d30c
zijie <68392315+zhu-zijie@users.noreply.github.com>
2023-11-03 17:33:43 +0800

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld{
-    public static void main(String[] args){
-        //System.out.println("HelleWorld!");
-        int i = 128;
-        byte j = (byte)i;
-        System.out.println(j);
-    }
-}
