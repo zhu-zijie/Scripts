08db1fa2c43f4032f3d3734a3af193d2f78325a6
zijie <68392315+zhu-zijie@users.noreply.github.com>
2024-09-10 10:13:05 +0800

    Update mgtv.js

--- QuantumultX/mgtv.js
+++ QuantumultX/mgtv.js
@@ -13,8 +13,8 @@
 [rewrite_local]
 
 # > 芒果TV,Vip+会员画质+去广告,页面优化
-^https?:\/\/.*mgtv.com.*(GetUserInfo|module\/list\?_support|app\/bdboot|pullReleaseInfo|floor\/detail|video\/source|entry\?_support).*$ url script-response-body https://raw.githubusercontent.com/WeiGiegie/666/main/mgtv.js
-^https?:\/\/mobile-stream\.api\.mgtv\.com\/v1\/video\/source\? url script-request-header https://raw.githubusercontent.com/WeiGiegie/666/main/mgtk.js
+^https?:\/\/.*mgtv.com.*(GetUserInfo|module\/list\?_support|app\/bdboot|pullReleaseInfo|floor\/detail|video\/source|entry\?_support).*$ url script-response-body https://raw.githubusercontent.com/zhu-zijie/Scripts/main/QuantumultX/mgtv.js
+^https?:\/\/mobile-stream\.api\.mgtv\.com\/v1\/video\/source\? url script-request-header https://raw.githubusercontent.com/zhu-zijie/Scripts/main/QuantumultX/mgtv.js
 ^https?://mobile.da.mgtv.com\/m\/page url reject
 
 [mitm] 
