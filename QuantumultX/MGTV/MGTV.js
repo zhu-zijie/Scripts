794e5e93c9c542f7b3cc9e41563342cd61106848
zhu-zijie <2672884775@qq.com>
2024-09-10 13:13:54 +0800

    fix

--- QuantumultX/MGTV/MGTV.js
+++ QuantumultX/MGTV/MGTV.js
@@ -8,8 +8,8 @@ https://raw.githubusercontent.com/RuCu6/QuanX/main/Rewrites/Cube/cnftp.snippet
 [rewrite_local]
 ^http[s]?:\/\/mobile\.api\.mgtv\.com\/v[0-9]\/(playlist|video\/album|video\/relative|video\/list).*$ url script-request-header https://raw.githubusercontent.com/zhu-zijie/Scripts/main/QuantumultX/MGTV/mgtv1.js
 https://mobile-stream.api.mgtv.com/v1/video/source? url script-request-header https://raw.githubusercontent.com/zhu-zijie/Scripts/main/QuantumultX/MGTV/MGTV.js
-https://nuc.api.mgtv.com/GetUserInfo url script-response-body https://raw.githubusercontent.com/zhu-zijie/Scripts/main/QuantumultX/MGTV/mgtv.js
-#https://mobile-stream.api.mgtv.com/v1/video/source url script-response-body https://raw.githubusercontent.com/zhu-zijie/Scripts/main/QuantumultX/MGTV/mgtv.js
+https://nuc.api.mgtv.com/GetUserInfo url script-response-body https://raw.githubusercontent.com/zhu-zijie/Scripts/main/QuantumultX/MGTV/mgtv3.js
+#https://mobile-stream.api.mgtv.com/v1/video/source url script-response-body https://raw.githubusercontent.com/zhu-zijie/Scripts/main/QuantumultX/MGTV/mgtv3.js
 #港区
 ^https://mobile.api.mgtv.com/v8/video/getSource url script-request-header https://raw.githubusercontent.com/zhu-zijie/Scripts/main/QuantumultX/MGTV/MGTV.js
 #播放页开通提示移除
