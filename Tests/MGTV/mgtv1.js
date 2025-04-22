e397fe7ef2344c5684cab91aeeb4b6e2c0db399f
zhuzijie <2672884775@qq.com>
2025-04-22 23:48:52 +0800

    refactor: simplify header processing in mgtv1.js by destructuring and removing unnecessary loops

--- Tests/MGTV/mgtv1.js
+++ Tests/MGTV/mgtv1.js
@@ -1,20 +1,2 @@
-// 获取原始请求头
-const originalHeaders = $request.headers;
-
-// 创建新的请求头对象
-const newHeaders = {};
-
-// 复制所有不需要过滤的请求头
-for (const headerName in originalHeaders) {
-  // 跳过需要移除的三个请求头
-  if (
-    headerName !== "Cookie" &&
-    headerName !== "x-r-i" &&
-    headerName !== "x-l-r-i"
-  ) {
-    newHeaders[headerName] = originalHeaders[headerName];
-  }
-}
-
-// 返回处理后的请求头
-$done({ headers: newHeaders });
+var { Cookie, "x-r-i": xri, "x-l-r-i": xlri, ...rest } = $request.headers;
+$done({ headers: rest });
