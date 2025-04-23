1d641490839f1eb61918bb375e5cddb3b0e6abe3
zhuzijie <2672884775@qq.com>
2025-04-23 15:45:07 +0800

    updata ztqs

--- Tests/ztqs.js
+++ Tests/ztqs.js
@@ -1,6 +1,6 @@
 /**********************************************
 [rewrite_local]
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body hhttps://raw.githubusercontent.com/zhu-zijie/Scripts/main/Tests/ztqs.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/zhu-zijie/Scripts/main/Tests/ztqs.js
 [mitm] 
 hostname = buy.itunes.apple.com
 **********************************************/
