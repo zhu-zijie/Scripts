5ad620e9ad2756e2b9a43b7594793029088b85d3
zhuzijie <2672884775@qq.com>
2025-10-28 00:52:32 +0800

    refactor: remove douyin video fetching script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
 
-// 自定义全局变量，用于代理工具脚本
 const proxyScriptGlobals = {
   $: "readonly",
   $persistentStore: "readonly",
